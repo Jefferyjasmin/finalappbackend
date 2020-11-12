ffd97726d6b8bee4a6d49885c497c8996cef9eba
Jefferyjasmin <Jefferyjasmin1@gmail.com>
2020-11-12 18:10:59 +0000

    comment

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Expense
 #from models import Person
 
 app = Flask(__name__)
@@ -31,15 +31,91 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_get():
+    users = User.query.all()
+    users = list(map(lambda x: x.serialize(), users))
+    return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:id>', methods=['GET'])
+def handle_user(id):
+    user = User.query.filter_by(id=id)
+    user = list(map(lambda x: x.serialize(), user))
+    return jsonify(user), 200
 
-# this only runs if `$ python src/main.py` is executed
-if __name__ == '__main__':
+
+
+@app.route('/user/<int:id>', methods=['PUT'])
+def handle_income(id):
+    body = request.get_json()
+    user = User.query.get(id)
+    # user = list(map(lambda x: x.serialize(), user))
+    user.income = body["income"]
+    db.session.commit()
+    user = User.query.filter_by(id=id)
+    user = list(map(lambda x: x.serialize(), user))
+    return jsonify(user),200
+
+
+
+
+@app.route('/user', methods=['POST'])
+def handle_post():
+    body = request.get_json()
+    user = User(email=body['email'],income=body['income'],user_name=body['userName'] , is_active=body['is_active'])
+    db.session.add(user)
+    db.session.commit()
+    user = User.query.filter_by(user_name=body['userName'])
+     
+    user = list(map(lambda x: x.serialize(), user))
+    return jsonify(user),200
+
+
+
+@app.route('/expense', methods=['POST'])
+def post_expense():
+    body = request.get_json()
+    expense = Expense(label=body['label'],value=body['value'])
+    db.session.add(expense)
+    db.session.commit()
+    expense = Expense.query.all()
+     
+    expense = list(map(lambda x: x.serialize(), expense))
+    return jsonify(expense),200
+
+
+
+@app.route('/expense', methods=['GET'])
+def get_expenses():
+    expenses = Expense.query.all()
+    expenses = list(map(lambda x: x.serialize(), expenses))
+    return jsonify(expenses),200
+
+
+
+
+
+
+
+@app.route('/expense/<int:id>', methods=['DELETE'])
+def delete_expense(id):
+    expense = Expense.query.get(id)
+    if expense is None: 
+        raise APIException("Expense doesnt exists",status_code=404)
+    db.session.delete(expense)
+    db.session.commit()
+    expenses = Expense.query.all()
+    expenses = list(map(lambda x: x.serialize(), expenses))
+    return jsonify(expenses),200
+
+
+
+
+
+
+
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
+
+
--- src/models.py
+++ src/models.py
@@ -5,15 +5,38 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    income = db.Column(db.Integer, unique=False, nullable =False)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.user_name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active": self.is_active,
+            "income": self.income,
+            "user_name": self.user_name
+            
+            # do not serialize the password, its a security breach
+        }
+
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=True, nullable=False)
+    value = db.Column(db.Integer, unique=False, nullable =False)
+
+    def __repr__(self):
+        return '<Expense %r>' % self.label
+    
+    def serialize(self):
+         return {
+            "id": self.id,
+            "value": self.value,   
+            "label": self.label
             # do not serialize the password, its a security breach
         }
